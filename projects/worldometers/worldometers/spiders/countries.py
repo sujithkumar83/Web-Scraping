4759bb3beef94cb1cff28760d6e462d6f4432481
Sujith Kumar <sujithkumar83@outlook.com>
2020-06-29 00:01:42 +0530

    Updated with Scrapy Shell example

--- projects/worldometers/worldometers/spiders/countries.py
+++ projects/worldometers/worldometers/spiders/countries.py
@@ -5,7 +5,7 @@ import scrapy
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info/world-population/population-by-country/']
-    start_urls = ['http://www.worldometers.info/world-population/population-by-country//']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country//']
 
     def parse(self, response):
         pass
