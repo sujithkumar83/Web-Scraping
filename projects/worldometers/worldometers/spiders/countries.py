f9f67a12327390119dc21b93e57444814719eb12
Sujith Kumar <sujithkumar83@outlook.com>
2020-06-28 23:05:18 +0530

    Starting a new scrapy project images

---
+++ projects/worldometers/worldometers/spiders/countries.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info/world-population/population-by-country/']
+    start_urls = ['http://www.worldometers.info/world-population/population-by-country//']
+
+    def parse(self, response):
+        pass
